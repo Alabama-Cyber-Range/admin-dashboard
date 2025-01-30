f50636f8f1e42d0df47d0cc86bd624f7cfb9e273
dsr0010 <63587300+dsr0010@users.noreply.github.com>
2025-01-30 12:25:15 -0600

    Update SchoolsTable.tsx

--- client/src/pages/module/SchoolsTable.tsx
+++ client/src/pages/module/SchoolsTable.tsx
@@ -24,7 +24,7 @@ const SchoolsTable = (props: LabData) => {
   const labId  = props.labId;
   const data = useLabSchools(labId);
     useEffect(() => {}
-    , [data, "labSchools"]);
+    , [data]);
   const navigate = useNavigate();
 
   const queryClient = useQueryClient()
